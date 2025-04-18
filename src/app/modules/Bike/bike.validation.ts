76c89b4451e626b14b8e3be2a13f18d36055579f
sopnilali <sopnilali24@gmail.com>
2025-04-18 19:55:47 +0600

    error fix

--- src/app/modules/Bike/bike.validation.ts
+++ src/app/modules/Bike/bike.validation.ts
@@ -4,7 +4,7 @@ const createBikeValidateSchema = z.object({
     body: z.object({
         brand: z.string({required_error: "Name is reqired", invalid_type_error: "Name must be string"}),
         model: z.string({required_error: "model is reqired", invalid_type_error: "model must be string"}),
-        year: z.string({required_error: "year is reqired", invalid_type_error: "year must be string"}),
+        year: z.number({required_error: "year is reqired", invalid_type_error: "year must be number"}),
         customerId: z.string({required_error: "Customer Id is reqired", invalid_type_error: "Customer Id must be string"})
     })
 })
